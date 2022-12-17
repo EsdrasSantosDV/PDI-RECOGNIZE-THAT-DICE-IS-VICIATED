4da22c8914884abf789df8bac91650aa36df9183
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-12-17 15:27:45 -0300

    feat:codigo funcionando

---
+++ codigofuncionando.py
@@ -0,0 +1,66 @@
+import cv2
+import numpy as np
+from collections import deque
+
+min_threshold = 10
+max_threshold = 200
+min_area = 60
+min_circularity = 0.3
+min_inertia_ratio = 0.5
+
+cap =cv2.VideoCapture('video9.mp4')
+cap.set(15, -4)
+
+
+counter = 0
+readings = deque([0, 0], maxlen=10)
+display = deque([0, 0], maxlen=10)
+
+while True:
+    ret, im = cap.read()
+    if ret == True:
+        params = cv2.SimpleBlobDetector_Params()
+        params.filterByArea = True
+        params.filterByCircularity = True
+        params.filterByInertia = True
+        params.minThreshold = min_threshold
+        params.maxThreshold = max_threshold
+        params.minArea = min_area
+        params.minCircularity = min_circularity
+        params.minInertiaRatio = min_inertia_ratio
+        params.filterByColor = False
+        detector = cv2.SimpleBlobDetector_create(params)
+
+        keypoints = detector.detect(im)
+
+
+
+        if len(keypoints):
+            im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0, 0, 255),
+                                                  cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+            cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow("frame", 360, 640)
+            cv2.imshow("frame", im_with_keypoints)
+
+
+        if counter % 10 == 0:
+            reading = len(keypoints)
+            readings.append(reading)
+
+            if readings[-1] == readings[-2] == readings[-3]:
+                display.append(readings[-1])
+
+
+            if display[-1] != display[-2] and display[-1] != 0:
+                msg = f"{display[-1]}\n****"
+                print(msg)
+
+        counter += 1
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+
+
+cv2.destroyAllWindows()
--- main3.py
+++ main3.py
@@ -5,38 +5,23 @@ from sklearn import cluster
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
 
+min_threshold = 10  # these values are used to filter our detector.
+max_threshold = 200  # they can be tweaked depending on the camera distance, camera angle, ...
+min_area = 100  # ... focus, brightness, etc.
+min_circularity = 0.3
+min_inertia_ratio = 0.5
 
 # Setting up the blob detector
 params = cv2.SimpleBlobDetector_Params()
 
-
-# images are converted to many binary b/w layers. Then 0 searches for dark blobs, 255 searches for bright blobs. Or you set the filter to "false", then it finds bright and dark blobs, both.
-params.filterByColor = False
-params.blobColor = 0
-params.minRepeatability = 5
-# Extracted blobs have an area between minArea (inclusive) and maxArea (exclusive).
 params.filterByArea = True
-params.minArea = 3. # Highly depending on image resolution and dice size
-params.maxArea = 400. # float! Highly depending on image resolution.
-
 params.filterByCircularity = True
-params.minCircularity = 0. # 0.7 could be rectangular, too. 1 is round. Not set because the dots are not always round when they are damaged, for example.
-params.maxCircularity = 3.4028234663852886e+38 # infinity.
-
-params.filterByConvexity = False
-params.minConvexity = 0.
-params.maxConvexity = 3.4028234663852886e+38
-
-params.filterByInertia = True # a second way to find round blobs.
-params.minInertiaRatio = 0.55 # 1 is round, 0 is anywhat
-params.maxInertiaRatio = 3.4028234663852886e+38 # infinity again
-
-params.minThreshold = 0 # from where to start filtering the image
-params.maxThreshold = 255.0 # where to end filtering the image
-params.thresholdStep = 5 # steps to go through
-params.minDistBetweenBlobs = 3.0 # avoid overlapping blobs. must be bigger than 0. Highly depending on image resolution!
-params.minRepeatability = 2 # if the same blob center is found at different threshold values (within a minDistBetweenBlobs), then it (basically) increases a counter for that blob. if the counter for each blob is >= minRepeatability, then it's a stable blob, and produces a KeyPoint, otherwise the blob is discarded.
-
+params.filterByInertia = True
+params.minThreshold = min_threshold
+params.maxThreshold = max_threshold
+params.minArea = min_area
+params.minCircularity = min_circularity
+params.minInertiaRatio = min_inertia_ratio
 detector = cv2.SimpleBlobDetector_create(params)
 
 
