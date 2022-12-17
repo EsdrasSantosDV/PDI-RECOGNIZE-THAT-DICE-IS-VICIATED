3e3ae2625e07669e24df60a19f6a9925c131a07f
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-12-17 11:42:52 -0300

    feat:adding recognize dice number

--- main.py
+++ main.py
@@ -1,16 +1,112 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import cv2
+import numpy as np
+from sklearn import cluster
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Setting up the blob detector
+params = cv2.SimpleBlobDetector_Params()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+params.filterByInertia
+params.minInertiaRatio = 0.6
+
+detector = cv2.SimpleBlobDetector_create(params)
+
+
+def get_blobs(frame):
+    frame_blurred = cv2.medianBlur(frame, 7)
+    frame_gray = cv2.cvtColor(frame_blurred, cv2.COLOR_BGR2GRAY)
+    blobs = detector.detect(frame_gray)
+
+    return blobs
+
+
+def get_dice_from_blobs(blobs):
+    # Get centroids of all blobs
+    X = []
+    for b in blobs:
+        pos = b.pt
+
+        if pos != None:
+            X.append(pos)
+
+    X = np.asarray(X)
+
+    if len(X) > 0:
+        # Important to set min_sample to 0, as a dice may only have one dot
+        clustering = cluster.DBSCAN(eps=40, min_samples=1).fit(X)
+
+        # Find the largest label assigned + 1, that's the number of dice found
+        num_dice = max(clustering.labels_) + 1
+
+        dice = []
+
+        # Calculate centroid of each dice, the average between all a dice's dots
+        for i in range(num_dice):
+            X_dice = X[clustering.labels_ == i]
+
+            centroid_dice = np.mean(X_dice, axis=0)
+
+            dice.append([len(X_dice), *centroid_dice])
+
+        return dice
+
+    else:
+        return []
+
+
+def overlay_info(frame, dice, blobs):
+    # Overlay blobs
+    for b in blobs:
+        pos = b.pt
+        r = b.size / 2
+
+        cv2.circle(frame, (int(pos[0]), int(pos[1])),
+                   int(r), (255, 0, 0), 2)
+
+    # Overlay dice number
+    for d in dice:
+        # Get textsize for text centering
+        textsize = cv2.getTextSize(
+            str(d[0]), cv2.FONT_HERSHEY_PLAIN, 3, 2)[0]
+
+        cv2.putText(frame, str(d[0]),
+                    (int(d[1] - textsize[0] / 2),
+                     int(d[2] + textsize[1] / 2)),
+                    cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
+
+
+cap = cv2.VideoCapture('videopdi.mp4')
+
+# Check if camera opened successfully
+if (cap.isOpened( )== False):
+    print("Error opening video stream or file")
+
+# Read until video is completed
+while(cap.isOpened()):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if ret == True:
+        # We'll define these later
+        blobs = get_blobs(frame)
+        dice = get_dice_from_blobs(blobs)
+        out_frame = overlay_info(frame, dice, blobs)
+        # Display the resulting frame
+        cv2.imshow('Frame' ,frame)
+
+        # Press Q on keyboard to  exit
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    # Break the loop
+    else:
+        break
+
+# When everything done, release the video capture object
+cap.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
+
